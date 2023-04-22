deadeb78ebbbf87e319326f07474f1028fe0c8ae
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-04-22 18:16:07 +0200

    camera moves

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import pygame
+import pygame_menu
 
 WIDTH, HEIGHT, = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((1500, 1000))
 pygame.display.set_caption("Game")
 FPS = 60
+BACKGROUND_SIZE = (3000, 2000)
+background = pygame.image.load("assets/map/map.png").convert()
+BACKGROUND = pygame.transform.scale(background, BACKGROUND_SIZE)
+CAM_SPEED = 5
+cam_pos_x = 0
+cam_pos_y = 0
+pygame.init()
 
 
 def draw_window():
+    WIN.blit(BACKGROUND, (cam_pos_x, cam_pos_y))
     pygame.display.update()
+
+
+def camera_moves():
+    global cam_pos_y
+    global cam_pos_x
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        cam_pos_y += CAM_SPEED
+    if keys[pygame.K_DOWN]:
+        cam_pos_y -= CAM_SPEED
+    if keys[pygame.K_RIGHT]:
+        cam_pos_x -= CAM_SPEED
+    if keys[pygame.K_LEFT]:
+        cam_pos_x += CAM_SPEED
 
 
 def main():
@@ -18,7 +41,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+            #
+        camera_moves()
+        print(cam_pos_y)
         draw_window()
 
     pygame.quit()
