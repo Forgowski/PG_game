7117d75eb353fd773437a3708fda264fce06e7a6
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-04-22 18:39:47 +0200

    cameras borders

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 import pygame_menu
 
-WIDTH, HEIGHT, = 900, 500
-WIN = pygame.display.set_mode((1500, 1000))
+WIDTH, HEIGHT, = 1200, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 FPS = 60
 BACKGROUND_SIZE = (3000, 2000)
 background = pygame.image.load("assets/map/map.png").convert()
 BACKGROUND = pygame.transform.scale(background, BACKGROUND_SIZE)
-CAM_SPEED = 5
+CAM_SPEED = 20
 cam_pos_x = 0
 cam_pos_y = 0
 pygame.init()
@@ -23,13 +23,13 @@ def camera_moves():
     global cam_pos_y
     global cam_pos_x
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and cam_pos_y + CAM_SPEED <= 0:
         cam_pos_y += CAM_SPEED
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and cam_pos_y - CAM_SPEED - HEIGHT >= -BACKGROUND_SIZE[1]:
         cam_pos_y -= CAM_SPEED
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and cam_pos_x - CAM_SPEED - WIDTH >= -BACKGROUND_SIZE[0]:
         cam_pos_x -= CAM_SPEED
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and cam_pos_x + CAM_SPEED <= 0:
         cam_pos_x += CAM_SPEED
 
 
@@ -43,7 +43,7 @@ def main():
                 run = False
             #
         camera_moves()
-        print(cam_pos_y)
+        print(f'{cam_pos_x} +" " +{cam_pos_y}')
         draw_window()
 
     pygame.quit()
